46668c0d2ebe1bf323e04ab208a72e19c13c6032
sima jadhav <simajadhav63@gmail.com>
2024-05-25 20:50:16 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BudgetProvider } from './context/budgetcontext';
+import Header from './componant/Header.js';
+import Budget from './componant/.budget.js';
+import ExpenseForm from './componant/expenseform';
+import ExpenseList from './componant/expenselist';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BudgetProvider>
+      <div className="App">
+        <Header />
+        <div className="container">
+          <Budget />
+          <ExpenseForm />
+          <ExpenseList />
+        </div>
+      </div>
+    </BudgetProvider>
   );
-}
+};
 
 export default App;
